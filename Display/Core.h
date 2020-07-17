36711cf33eb7ffddf95f7d4f0c9de0bff18ad26b
Jonathan Ho <jonafanho@gmail.com>
2020-07-17 09:25:41 -0700

    Reformatted for CLion.

--- Display/Core.h
+++ Display/Core.h
@@ -1,30 +1,27 @@
 #ifndef CORE_H
 #define CORE_H
+
 #include "Arduino.h"
 #include "SdFat.h"
 #include "U8x8lib.h"
 
-template <class T>
-class Core
-{
+template<class T>
+class Core {
 public:
 	T display;
 
 	Core(const uint16_t width, const uint16_t height, const uint8_t cs, const uint8_t dc) : WIDTH(width), HEIGHT(height), display(T(cs, dc)) {}
 
-	void loadBmp()
-	{
+	void loadBmp() {
 		// Copy old image
-		for (uint16_t i = 0; i < IMAGE_BUFFER_SIZE; i++)
-		{
+		for (uint16_t i = 0; i < IMAGE_BUFFER_SIZE; i++) {
 			image[IMAGE_BUFFER_SIZE + i] = image[i];
 			image[i] = 0;
 		}
 
 		// Read file
 		File bmpFile;
-		if (!bmpFile.openNext(&folder))
-		{
+		if (!bmpFile.openNext(&folder)) {
 			folder.rewindDirectory();
 			if (!bmpFile.openNext(&folder))
 				return;
@@ -38,14 +35,11 @@ public:
 			fancyScroll = true;
 
 			// Read left offset and divide
-			if (fileName[1] >= 48 && fileName[1] < 58 && fileName[2] >= 48 && fileName[2] < 58 && fileName[3] >= 48 && fileName[3] < 58)
-			{
+			if (fileName[1] >= 48 && fileName[1] < 58 && fileName[2] >= 48 && fileName[2] < 58 && fileName[3] >= 48 && fileName[3] < 58) {
 				fancyScrollDivide = (fileName[1] - 48) * 100 + (fileName[2] - 48) * 10 + fileName[3] - 48;
 				fancyScrollLeftOffset = fancyScrollDivide % 8;
 				fancyScrollDivide = (fancyScrollDivide + 7) / 8;
-			}
-			else
-			{
+			} else {
 				fancyScrollLeftOffset = 0;
 				fancyScrollDivide = 6;
 			}
@@ -53,19 +47,17 @@ public:
 			// Clear old image
 			for (uint16_t i = 0; i < IMAGE_BUFFER_SIZE; i++)
 				image[IMAGE_BUFFER_SIZE + i] = 0;
-		}
-		else
-		{
+		} else {
 			fancyScroll = false;
 		}
 
 		// Parse BMP header
 		if (read16(bmpFile) != 0x4D42) // BMP signature
 			return;
-		read32(bmpFile);					  // File size
-		read32(bmpFile);					  // Reserved
+		read32(bmpFile);                      // File size
+		read32(bmpFile);                      // Reserved
 		uint32_t bmpOffset = read32(bmpFile); // Start of image data
-		read32(bmpFile);					  // Number of bytes in DIB header
+		read32(bmpFile);                      // Number of bytes in DIB header
 		int32_t bmpWidth = read32(bmpFile);
 		int32_t bmpHeight = read32(bmpFile);
 		if (read16(bmpFile) != 1) // Planes: must be 1
@@ -79,8 +71,7 @@ public:
 		// BMP rows are padded (if needed) to 4-byte boundary
 		uint32_t rowSize = ((bmpWidth * bytesPerPixel + 3) / 4) * 4;
 		boolean flip = true; // BMP is stored bottom-to-top
-		if (bmpHeight < 0)
-		{
+		if (bmpHeight < 0) {
 			bmpHeight = -bmpHeight;
 			flip = false;
 		}
@@ -98,8 +89,7 @@ public:
 
 		uint8_t sdBuffer[SD_BUFFER_SIZE];
 		uint8_t bufferIndex = SD_BUFFER_SIZE;
-		for (uint16_t row = bmpReadTop; row < bmpReadBottom; row++)
-		{
+		for (uint16_t row = bmpReadTop; row < bmpReadBottom; row++) {
 			if (scale != 1 && row % scale == scale - 1)
 				continue;
 
@@ -110,21 +100,16 @@ public:
 				bufferIndex = SD_BUFFER_SIZE; // Force buffer reload
 			}
 
-			for (uint16_t column = bmpReadLeft; column < bmpReadRight; column++)
-			{
+			for (uint16_t column = bmpReadLeft; column < bmpReadRight; column++) {
 				// Time to read more pixel data?
-				if (bufferIndex >= SD_BUFFER_SIZE)
-				{
+				if (bufferIndex >= SD_BUFFER_SIZE) {
 					bmpFile.read(sdBuffer, SD_BUFFER_SIZE);
 					bufferIndex = 0; // Set index to beginning
 				}
 
-				if (scale != 1 && column % scale == scale - 1)
-				{
+				if (scale != 1 && column % scale == scale - 1) {
 					bufferIndex += bytesPerPixel;
-				}
-				else
-				{
+				} else {
 					uint16_t screenDrawX = (column - bmpReadLeft) / scale + screenWriteColumnStart;
 					uint16_t screenDrawY = (row - bmpReadTop) / scale + screenWriteRowStart;
 					uint16_t arrayIndex;
@@ -133,13 +118,10 @@ public:
 					if (fancyScroll && screenDrawX >= fancyScrollDivide * 8)
 						screenDrawX += 8;
 
-					if (XBM_MODE)
-					{
+					if (XBM_MODE) {
 						arrayIndex = screenDrawX / 8 + (screenDrawY * WIDTH * (fancyScroll ? 2 : 1) + 7) / 8;
 						arrayBit = screenDrawX % 8;
-					}
-					else
-					{
+					} else {
 						arrayIndex = (screenDrawX % (WIDTH * (fancyScroll ? 2 : 1))) + screenDrawY / 8 * WIDTH * (fancyScroll ? 2 : 1);
 						arrayBit = screenDrawY % 8;
 					}
@@ -148,27 +130,26 @@ public:
 						arrayIndex = IMAGE_BUFFER_SIZE * (fancyScroll ? 2 : 1) - 1;
 
 					uint16_t p, r, g, b;
-					switch (bytesPerPixel)
-					{
-					case 1:
-						p = sdBuffer[bufferIndex++];
-						r = ((p & 0b00110000) >> 4) * 64;
-						g = ((p & 0b00001100) >> 2) * 64;
-						b = (p & 0b00000011) * 64;
-						break;
-					case 2:
-						p = (sdBuffer[bufferIndex++] << 8) + sdBuffer[bufferIndex++];
-						r = ((p & 0b0000111100000000) >> 8) * 16;
-						g = ((p & 0b0000000011110000) >> 4) * 16;
-						b = (p & 0b0000000000001111) * 16;
-						break;
-					case 4:
-						bufferIndex++;
-					case 3:
-						b = sdBuffer[bufferIndex++];
-						g = sdBuffer[bufferIndex++];
-						r = sdBuffer[bufferIndex++];
-						break;
+					switch (bytesPerPixel) {
+						case 1:
+							p = sdBuffer[bufferIndex++];
+							r = ((p & 0b00110000) >> 4) * 64;
+							g = ((p & 0b00001100) >> 2) * 64;
+							b = (p & 0b00000011) * 64;
+							break;
+						case 2:
+							p = (sdBuffer[bufferIndex++] << 8) + sdBuffer[bufferIndex++];
+							r = ((p & 0b0000111100000000) >> 8) * 16;
+							g = ((p & 0b0000000011110000) >> 4) * 16;
+							b = (p & 0b0000000000001111) * 16;
+							break;
+						case 4:
+							bufferIndex++;
+						case 3:
+							b = sdBuffer[bufferIndex++];
+							g = sdBuffer[bufferIndex++];
+							r = sdBuffer[bufferIndex++];
+							break;
 					}
 
 					if (bytesPerPixel == 1) // Might find a better solution here later
@@ -182,13 +163,11 @@ public:
 		bmpFile.close();
 	}
 
-	void draw()
-	{
+	void draw() {
 		const uint16_t widthChunk = WIDTH / 8;
 		const uint16_t heightChunk = HEIGHT / 8;
 
-		if (fancyScroll)
-		{
+		if (fancyScroll) {
 			// Fancy scrolling
 			display.clearDisplay();
 
@@ -199,8 +178,7 @@ public:
 
 			const uint16_t fancyScrollDivideChunk = fancyScrollDivide * 8;
 
-			for (uint16_t step = WIDTH - 1; step >= fancyScrollDivideChunk; step--)
-			{
+			for (uint16_t step = WIDTH - 1; step >= fancyScrollDivideChunk; step--) {
 				const uint16_t startTile = step / 8;
 				const uint16_t numTiles = widthChunk - startTile;
 				uint8_t *imagePointer = image + fancyScrollDivideChunk + 7 - step % 8;
@@ -210,35 +188,29 @@ public:
 
 			const uint16_t numTiles = widthChunk - fancyScrollDivide;
 			const uint16_t remainingWidth = 512 - WIDTH - 8;
-			for (uint16_t step = 0; step < remainingWidth; step++)
-			{
+			for (uint16_t step = 0; step < remainingWidth; step++) {
 				uint8_t *imagePointer = image + fancyScrollDivideChunk + step + 8;
 				for (uint16_t row = 0; row < heightChunk; row++)
 					display.drawTile(fancyScrollDivide, row, numTiles, imagePointer + row * doubleWidth);
 			}
-		}
-		else
-		{
+		} else {
 			// Normal image display
 			for (uint16_t row = 0; row < heightChunk; row++)
 				display.drawTile(0, row, widthChunk, image + row * WIDTH);
 		}
 	}
 
-	void step(const uint16_t step)
-	{
+	void step(const uint16_t step) {
 		uint8_t stepMod = step % 8, stepRounded = step - stepMod;
 		uint8_t tile[8];
-		for (uint16_t y = 0; y < HEIGHT / 8; y++)
-		{
+		for (uint16_t y = 0; y < HEIGHT / 8; y++) {
 			for (uint8_t i = 0; i < 8; i++)
 				tile[i] = image[(i <= stepMod ? 0 : IMAGE_BUFFER_SIZE) + stepRounded + i + y * WIDTH];
 			display.drawTile(step / 8, y, 1, tile);
 		}
 	}
 
-	boolean setFolder(const char *newFolder)
-	{
+	boolean setFolder(const char *newFolder) {
 		folder.close();
 		folder.open(newFolder);
 
@@ -250,8 +222,7 @@ public:
 		return empty;
 	}
 
-	void setScaleCrop(const uint8_t s, const uint8_t c)
-	{
+	void setScaleCrop(const uint8_t s, const uint8_t c) {
 		if (s > 0)
 			scale = s;
 		crop = c;
@@ -261,7 +232,8 @@ private:
 	uint8_t scale = 1, crop = 0, fancyScrollLeftOffset;
 	uint16_t fancyScrollDivide;
 	boolean fancyScroll = false;
-	U8X8_PROGMEM uint8_t image[4096] = {}; // first 2048 bytes for new image, next 2048 bytes for old image (except for scrolling)
+	U8X8_PROGMEM uint8_t
+	image[4096] = {}; // first 2048 bytes for new image, next 2048 bytes for old image (except for scrolling)
 	File folder;
 
 	const uint16_t WIDTH, HEIGHT, IMAGE_BUFFER_SIZE = 2048;
@@ -269,21 +241,19 @@ private:
 	static const uint8_t SD_BUFFER_SIZE = 3 * 20;
 	static const boolean XBM_MODE = false;
 
-	static uint16_t read16(File &f)
-	{
+	static uint16_t read16(File &f) {
 		uint16_t result;
-		((uint8_t *)&result)[0] = f.read(); // LSB
-		((uint8_t *)&result)[1] = f.read(); // MSB
+		((uint8_t * ) & result)[0] = f.read(); // LSB
+		((uint8_t * ) & result)[1] = f.read(); // MSB
 		return result;
 	}
 
-	static uint32_t read32(File &f)
-	{
+	static uint32_t read32(File &f) {
 		uint32_t result;
-		((uint8_t *)&result)[0] = f.read(); // LSB
-		((uint8_t *)&result)[1] = f.read();
-		((uint8_t *)&result)[2] = f.read();
-		((uint8_t *)&result)[3] = f.read(); // MSB
+		((uint8_t * ) & result)[0] = f.read(); // LSB
+		((uint8_t * ) & result)[1] = f.read();
+		((uint8_t * ) & result)[2] = f.read();
+		((uint8_t * ) & result)[3] = f.read(); // MSB
 		return result;
 	}
 };
--- Display/Display.ino
+++ Display/Display.ino
@@ -4,7 +4,7 @@ Core<U8X8_SSD1322_NHD_256X64_4W_HW_SPI> front(256, 64, PA9, PA8);
 Core<U8X8_SSD1322_NHD_256X64_4W_HW_SPI> side(256, 64, PA10, PA8);
 Core<U8X8_SH1106_128X64_VCOMH0_4W_HW_SPI> back(128, 64, PA11, PA8);
 
-SdFatSoftSpiEX<PB9, PB4, PB5> sd; // MISO, MOSI, CLK
+SdFatSoftSpiEX <PB9, PB4, PB5> sd; // MISO, MOSI, CLK
 File frontFolder, sideFolder, backFolder;
 
 uint8_t currentFolder = 0, switchDelay = 2, scrollMode = 1;
@@ -12,22 +12,21 @@ uint32_t ticks = 0;
 
 const uint8_t MAX_FOLDERS = 10, SETTINGS_COUNT = 6, SETTINGS_MAIN_COUNT = 2;
 const String SETTINGS[] = {
-	"front_scale=",
-	"front_crop=",
-	"side_scale=",
-	"side_crop=",
-	"back_scale=",
-	"back_crop=",
+		"front_scale=",
+		"front_crop=",
+		"side_scale=",
+		"side_crop=",
+		"back_scale=",
+		"back_crop=",
 };
 const String SETTINGS_MAIN[] = {
-	"scroll_animation=",
-	"switch_delay=",
+		"scroll_animation=",
+		"switch_delay=",
 };
 const uint8_t DEFAULT_SETTINGS[] = {3, 20, 3, 19, 1, 0};
 const uint8_t DEFAULT_SETTINGS_MAIN[] = {1, 2};
 
-void writeDefaultSettings(const char *name, const String *settings, const uint8_t *defaultSettings, const uint8_t settingsCount)
-{
+void writeDefaultSettings(const char *name, const String *settings, const uint8_t *defaultSettings, const uint8_t settingsCount) {
 	File settingsFile;
 	settingsFile.open(name, O_WRONLY | O_CREAT | O_TRUNC);
 	for (uint8_t i = 0; i < settingsCount; i++)
@@ -36,23 +35,19 @@ void writeDefaultSettings(const char *name, const String *settings, const uint8_
 	settingsFile.close();
 }
 
-void readSettings(const char *name, uint8_t *settingsOut, const String *settings, const uint8_t *defaultSettings, const uint8_t settingsCount)
-{
+void readSettings(const char *name, uint8_t *settingsOut, const String *settings, const uint8_t *defaultSettings, const uint8_t settingsCount) {
 	File settingsFile;
 	boolean validFile = true;
-	if (settingsFile.open(name, O_RDONLY))
-	{
+	if (settingsFile.open(name, O_RDONLY)) {
 		char buffer[128];
 		settingsFile.read(buffer, 128);
 		String text(buffer);
 
-		for (uint8_t i = 0; i < settingsCount; i++)
-		{
+		for (uint8_t i = 0; i < settingsCount; i++) {
 			settingsOut[i] = defaultSettings[i];
 
 			int8_t settingIndex = text.indexOf(settings[i]);
-			if (settingIndex < 0)
-			{
+			if (settingIndex < 0) {
 				validFile = false;
 				break;
 			}
@@ -60,8 +55,7 @@ void readSettings(const char *name, uint8_t *settingsOut, const String *settings
 			text = text.substring(settingIndex + settings[i].length());
 
 			int8_t crlfIndex = text.indexOf("\r\n");
-			if (crlfIndex < 0)
-			{
+			if (crlfIndex < 0) {
 				validFile = false;
 				break;
 			}
@@ -70,9 +64,7 @@ void readSettings(const char *name, uint8_t *settingsOut, const String *settings
 		}
 
 		settingsFile.close();
-	}
-	else
-	{
+	} else {
 		validFile = false;
 	}
 
@@ -80,12 +72,10 @@ void readSettings(const char *name, uint8_t *settingsOut, const String *settings
 		writeDefaultSettings(name, settings, defaultSettings, settingsCount);
 }
 
-void openAndIncrementFolder()
-{
+void openAndIncrementFolder() {
 	char name[32];
 	boolean frontEmpty = true, sideEmpty = true, backEmpty = true;
-	while (frontEmpty && sideEmpty && backEmpty)
-	{
+	while (frontEmpty && sideEmpty && backEmpty) {
 		sprintf(name, "%d/front", currentFolder);
 		frontEmpty = front.setFolder(name);
 
@@ -110,24 +100,20 @@ void openAndIncrementFolder()
 	back.setScaleCrop(settingValues[4], settingValues[5]);
 }
 
-void setup()
-{
+void setup() {
 	front.display.begin();
 	side.display.begin();
 	back.display.begin();
 
-	if (!sd.begin(PB6))
-	{
+	if (!sd.begin(PB6)) {
 		front.display.setFont(u8x8_font_5x7_f);
 		front.display.drawString(0, 0, "SD card initialization failed.");
-		while (true)
-		{
+		while (true) {
 		}
 	}
 
 	// default folders and settings files
-	for (uint8_t i = 0; i < MAX_FOLDERS; i++)
-	{
+	for (uint8_t i = 0; i < MAX_FOLDERS; i++) {
 		char name[32];
 
 		sprintf(name, "%d/front", i);
@@ -153,35 +139,30 @@ void setup()
 	openAndIncrementFolder();
 }
 
-void loop()
-{
-	for (uint8_t j = 0; j < 3; j++)
-	{
+void loop() {
+	for (uint8_t j = 0; j < 3; j++) {
 		front.loadBmp();
 		side.loadBmp();
 		back.loadBmp();
 
-		while (ticks != 0 && (millis() - ticks) / 1000 < (uint32_t)switchDelay)
-		{
+		while (ticks != 0 && (millis() - ticks) / 1000 < (uint32_t) switchDelay) {
 		}
 		ticks = millis();
 
-		switch (scrollMode)
-		{
-		case 0:
-			front.draw();
-			back.draw();
-			side.draw();
-			break;
-		case 1:
-			for (uint16_t i = 0; i < 256; i++)
-			{
-				front.step(i);
-				side.step(i);
-				back.step(i);
-				delay(2);
-			}
-			break;
+		switch (scrollMode) {
+			case 0:
+				front.draw();
+				back.draw();
+				side.draw();
+				break;
+			case 1:
+				for (uint16_t i = 0; i < 256; i++) {
+					front.step(i);
+					side.step(i);
+					back.step(i);
+					delay(2);
+				}
+				break;
 		}
 	}
 
