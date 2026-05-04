f6115dc2c819eaa4fc22ae8773c2b3064b039003
Jonathan Ho <jonafanho@gmail.com>
2026-05-03 23:41:30 -0700

    Use preprocessors to import the correct display driver

--- esp32/display/src/main.cpp
+++ esp32/display/src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 #include <init_print.h>
 #include <spi_slave.h>
-#include <ssd1322.h>
-#include <ssd1327.h>
 #include <display_transaction_slave.h>
 #include <stream_reader.h>
 
+#define DISPLAY_SSD1327
+#define DISPLAY_ROTATED false
+#define DISPLAY_HAS_WIPE true
+#define DISPLAY_PIXELATED_TYPE 0
+
+#if defined(DISPLAY_SSD1327)
+#include <ssd1327.h>
+SSD1327 displayDriver(DISPLAY_ROTATED, DISPLAY_HAS_WIPE, DISPLAY_PIXELATED_TYPE);
+#elif defined(DISPLAY_SSD1322)
+#include <ssd1322.h>
+SSD1322 displayDriver(DISPLAY_ROTATED, DISPLAY_HAS_WIPE, DISPLAY_PIXELATED_TYPE);
+#endif
+
 InitPrint initPrint("Display");
 SPISlave spiSlave;
-SSD1322 ssd1322(false, false, 0);
-SSD1327 ssd1327(false, false, 0);
 DisplayTransactionSlave displayTransactionSlave;
 StreamReader streamReader;
 
@@ -28,12 +37,12 @@ void setup()
 	Serial.println("");
 
 	initPrint.init(spiSlave.init(), "SPI device");
-	initPrint.init(ssd1322.init(), "SSD1322");
+	initPrint.init(displayDriver.init(), "Display driver");
 
 	xTaskCreatePinnedToCore(spiTask, "SPI", 4096, NULL, 2, NULL, 0);
 }
 
 void loop()
 {
-	displayTransactionSlave.tick(&spiSlave, &streamReader, &ssd1322);
+	displayTransactionSlave.tick(&spiSlave, &streamReader, &displayDriver);
 }
